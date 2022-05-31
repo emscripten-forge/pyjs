bc34b44a8525bc03d3ccffa6bacf0e1b0aca0272
DerThorsten <derthorstenbeier@gmail.com>
2022-05-31 09:24:09 +0200

    header only atm

--- include/pyjs/export_pyjs_module.hpp
+++ include/pyjs/export_pyjs_module.hpp
@@ -1,6 +1,29 @@
 #include <pybind11/pybind11.h>
 
+
+#include <emscripten.h>
+#include <emscripten/val.h>
+#include <emscripten/bind.h>
+
+#include <iostream>
+#include <filesystem>
+
+#include <pyjs/export_js_proxy.hpp>
+#include <pyjs/pure_python_init.hppy>
+#include <pyjs/macro_magic.hpp>
+
+
+
+namespace py = pybind11;
+namespace em = emscripten;
+
 namespace pyjs
 {
-    void export_pyjs_module(pybind11::module_ & pyjs_module);
+
+void export_pyjs_module(py::module_ & pyjs_module)
+{  
+    export_js_proxy(pyjs_module);
+    PYTHON_INIT(pyjs)(pyjs_module);
+}
+
 }

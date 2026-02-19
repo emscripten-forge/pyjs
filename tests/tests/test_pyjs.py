8c1c3a7f5c473794985663b140024aa96284267e
Thorsten Beier <derthorstenbeier@gmail.com>
2026-02-19 09:05:37 +0100

    Add test for fcntl functionality

--- tests/tests/test_pyjs.py
+++ tests/tests/test_pyjs.py
@@ -693,6 +693,9 @@ def test_imports_sys():
     import pexpect
     import resource
 
+def test_fcntl():
+    import os, fcntl
+    fcntl.fcntl(1, fcntl.F_GETFL)
 
 def test_webbrowser():
     from webbrowser import open, open_new, open_new_tab
